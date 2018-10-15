ff230c9245b14ecc88235a7200bed20698a1298c
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-15 20:23:46 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,9 +5,11 @@ class Hash
   def keys_of(args)
     result = []
     self.each do |k,v|
-      
-       if v == args
+      if args.count > 1
+        args.each{|el| result << k  if v == el}
+       elsif v == args
         result << k
+      end
     end
     result
   end
